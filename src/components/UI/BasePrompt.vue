e0c403e0e9daa470ed4269a442e3de2f06c80b44
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-08-22 19:57:24 +0300

    added prompt component

--- src/components/UI/BasePrompt.vue
+++ src/components/UI/BasePrompt.vue
@@ -1,5 +1,35 @@
+<script lang="ts" setup>
+
+const props: any = defineProps(['visibility'])
+
+</script>
+
+
 <template lang="pug">
 
-blockquote bruh
+transition( name="slide-fade" )
+   .prompt( v-if="visibility" )
+      slot
 
 </template>
+
+
+<style lang="scss" scoped>
+
+.prompt {
+   background: var(--color-background);
+   box-shadow: 0 0 20em 2em #000;
+
+   border-radius: 2em;
+   padding: 2em;
+
+   position: fixed;
+   top: 50%;
+   left: 50%;
+   transform: translate(-50%, -50%);
+
+   width: 70%;
+   height: 70%;
+}
+
+</style>
